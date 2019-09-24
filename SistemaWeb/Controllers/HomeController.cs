90cad8feba1b327ba1b16d6366248e96715f8396
Bruno Gomes Ferreira <brunoo.gomes11@gmail.com>
2019-09-24 18:54:21 -0300

    First controller and Razor pages tests

--- SistemaWeb/Controllers/HomeController.cs
+++ SistemaWeb/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SistemaWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Bruno Gomes";
 
             return View();
         }
